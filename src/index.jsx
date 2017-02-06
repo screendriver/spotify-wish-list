b1496202744e6ea0d9aa6675bbf0fd1c2e4a9f79
Christian Rackerseder <git@echooff.de>
2017-02-06 10:42:27 +0100

    Fixed ESLint

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
 import 'purecss';
+import Inferno, { render } from 'inferno';
 
-import { render } from 'inferno';
+import './styles/main.scss';
+
+const MyComponent = () => (
+  <div>MyComponent</div>
+);
 
 render(<MyComponent />, document.getElementById('root'));
