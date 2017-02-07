896265d13ea5e46e3be268aaa1d4cc168d683570
Christian Rackerseder <git@echooff.de>
2017-02-07 09:32:27 +0100

    Enabled Redux DevTools Extension

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import 'purecss';
 import Inferno, { render } from 'inferno';
 import { Provider } from 'inferno-redux';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
 
 import './styles/main.scss';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(reducers, devToolsEnhancer());
 
 function renderRoot() {
   render(
